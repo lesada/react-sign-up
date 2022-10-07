a35985936df44ac8640bbc45703c6ccdd904ef62
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2022-10-06 21:59:22 -0300

    Finishing SideBar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Form } from "./styles";
+import { Container, Form, Terms } from "./styles";
 import Input from "../Input";
 
 const SideBar = () => {
   return (
     <Container>
       <h1>
-        Lesada <span>Codes</span>
+        &lt; Lesada / &gt; <span>Codes</span>
       </h1>
       <Form>
         <h2>Sign Up</h2>
@@ -14,6 +14,14 @@ const SideBar = () => {
         <Input type="email" placeholder="Email" required />
         <Input type="password" placeholder="Password" required />
         <Input type="password" placeholder="Confirm Password" required />
+        <button>Sign Up</button>
+        <Terms>
+          By signing up, I agree to the Privacy Policy <br /> and Terms of
+          Service
+        </Terms>
+        <p>
+          Already have an account? <a href="#">Sign In</a>
+        </p>
       </Form>
     </Container>
   );
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -4,9 +4,11 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   /* flex: 1; */
 
-  background-color: #ffffffde;
+  background: rgba(255, 255, 255, 0.87);
+  backdrop-filter: blur(24px);
   height: 100vh;
   width: 400px;
 
@@ -14,7 +16,7 @@ export const Container = styled.div`
     font-size: 28px;
     font-weight: 700;
     color: #ff8d8d;
-    margin: 6rem 0 2rem;
+    margin-bottom: 2rem;
   }
 
   span {
@@ -22,16 +24,61 @@ export const Container = styled.div`
     font-weight: 400;
     color: #5dc399;
   }
+
+  p {
+    font-size: 15px;
+    font-weight: 500;
+    color: #808080;
+    margin-top: 1rem;
+
+    a {
+      color: #ff8d8d;
+      text-decoration: none;
+
+      &:hover {
+        filter: brightness(1.2);
+      }
+    }
+  }
 `;
 
 export const Form = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   h2 {
     color: #666666;
     font-weight: 700;
     margin-bottom: 1rem;
   }
+
+  button {
+    align-self: start;
+    width: 282px;
+    height: 60px;
+    margin: 1rem 0;
+
+    background-color: #70edb9;
+    color: #fff;
+    font-weight: 700;
+    font-size: 21px;
+
+    border: none;
+    border-radius: 8px;
+    box-shadow: 0px 14px 9px -15px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+
+    transition: transform 0.2s ease-in;
+    &:hover {
+      transform: translateY(-3px);
+    }
+  }
+`;
+
+export const Terms = styled.div`
+  color: #808080;
+  font-size: 15px;
+  font-weight: 400;
+  text-align: center;
+  padding: 0 1rem;
 `;
